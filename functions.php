7b708c9d31ded12db5ea3ecf025ccca858f37a5d
Al Amin Ahamed <34349365+mralaminahamed@users.noreply.github.com>
2022-09-12 22:33:41 +0600

    update: codebase

--- functions.php
+++ functions.php
@@ -1,7 +1,14 @@
 <?php
-	add_action( 'wp_enqueue_scripts', 'theme_enqueue_styles' );
-	function theme_enqueue_styles() {
-	    wp_enqueue_style( 'divi', get_template_directory_uri() . '/style.css' );
-      wp_enqueue_script( 'divi', plugin_dir_url( __FILE__ ) . 'js/scripts.js', array( 'jquery', 'divi-custom-script' ), '0.1.2', true );
-	}
-?>
+
+if (!function_exists('theme_enqueue_styles')):
+    /**
+     * @return void
+     */
+    function theme_enqueue_styles()
+    {
+        wp_enqueue_style('divi', get_template_directory_uri() . '/style.css');
+        wp_enqueue_script('divi', plugin_dir_url(__FILE__) . 'js/scripts.js', array('jquery', 'divi-custom-script'), '0.1.2', true);
+    }
+
+    add_action('wp_enqueue_scripts', 'theme_enqueue_styles');
+endif;
